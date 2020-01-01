0cfea8090879bcb02874db03b69d01173ddba653
Vikram Bageja <vikram.bageja@gmail.com>
2020-01-01 17:59:29 +0000

    add

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -3,9 +3,11 @@ import styled from "styled-components";
 import Button from "../common/Button";
 import Dialog from "../common/Dialog";
 import api from "../../api/api";
+import { withRouter } from 'react-router-dom'
 
-const Header = ({ game, player, getPlayer }) => {
+const Header = ({ game, player, getPlayer, history }) => {
   const [resetDialog, setResetDialog] = useState(false);
+  const [leaveDialog, setLeaveDialog] = useState(false);
 
   const resetAmount = e => {
     e.preventDefault();
@@ -13,6 +15,13 @@ const Header = ({ game, player, getPlayer }) => {
     api.updatePlayer(player.id, { amount: 1500, deposit: 0 }).then(getPlayer);
     api.updateGame(game.id, { free_parking: 0, history: [] });
   };
+
+  const leaveGame = e => {
+    e.preventDefault();
+    setLeaveDialog(true);
+    api.updatePlayer(player.id, { game: null}).then(getPlayer)
+    .then(() => history.push('/'))
+  }
 
   return (
     <>
@@ -23,15 +32,22 @@ const Header = ({ game, player, getPlayer }) => {
             alt="Monopoly"
           ></Logo>
         </LogoWrapper>
-        <ResetButton>
-          <Button
-            bg={"#30263F"}
-            padding={"7px"}
-            onClick={() => setResetDialog(true)}
-          >
-            RESET
+        <div className="top-buttons">
+          <ButtonContainer>
+          <Button bg={"#a20b0b"} className="leave-game" onClick={() => setLeaveDialog(true)}>
+            LEAVE
           </Button>
-        </ResetButton>
+          </ButtonContainer>
+          <ButtonContainer>
+            <Button
+              bg={"#30263F"}
+              onClick={() => setResetDialog(true)}
+            >
+              RESET
+            </Button>
+          </ButtonContainer>
+        </div>
+
       </HeaderWrapper>
       <Dialog open={resetDialog} closeFunction={() => setResetDialog(false)}>
         <form onSubmit={resetAmount} className="yes-cancel">
@@ -42,6 +58,20 @@ const Header = ({ game, player, getPlayer }) => {
           <div className="buttons">
             <Button className="yes">YES</Button>
             <Button className="cancel" onClick={() => setResetDialog(false)}>
+              NO
+            </Button>
+          </div>
+        </form>
+      </Dialog>
+      <Dialog open={leaveDialog} closeFunction={() => setLeaveDialog(false)}>
+        <form onSubmit={leaveGame} className="yes-cancel">
+          <div className="message">
+            <p>You're about to leave this game: {game.name}</p>
+            <p style={{ fontWeight: 1000 }}>Are you sure?</p>
+          </div>
+          <div className="buttons">
+            <Button className="yes">YES</Button>
+            <Button className="cancel" onClick={() => setLeaveDialog(false)}>
               NO
             </Button>
           </div>
@@ -59,13 +89,15 @@ const LogoWrapper = styled.div`
   margin-bottom: 10px;
 `;
 
-const ResetButton = styled.div`
+const ButtonContainer = styled.div`
   margin-top: 5px;
+  margin-left:5px;
 `;
+
 
 const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
-export default Header;
+export default withRouter(Header)
