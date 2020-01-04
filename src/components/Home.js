26027541fb18bb82d5a916a5ae860fdf395dde9d
Vikram Bageja <vikram.bageja@gmail.com>
2020-01-04 18:13:05 +0000

    log out added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Button from './common/Button'
 import styled from 'styled-components'
 
+import PlayerCard from '../components/Dashboard/PlayerCard'
 import api from '../api/api'
+import Auth from '../lib/auth'
 
 const HomeButton = styled(Button)`
   width: 80px;
 `
+const Home = ({ history }) => {
 
-const Home = () => {
+  const [player, setPlayer] = useState(null)
 
-  // useEffect(() => {
-  //   api.getPlayer()
-  //     .then(player => console.log(player))
-  //     .catch(err => console.log(err.response.data))
-  // }, [])
+  const logOut = () => {
+    Auth.logOut()
+    history.push('/')
+  }
 
+  useEffect(() => {
+    api.getPlayer(Auth.getToken())
+      .then(player => setPlayer(player))
+      .catch(err => console.log(err.response.data))
+  }, [])
+
+  if (!player) return null
   return (
     <div className="container">
       <div className="home-page">
-        <Link to="/join-game"><HomeButton bg="green">Join Game</HomeButton></Link>
-        <Link to="/create-game"><HomeButton>Create Game</HomeButton></Link>
+        <div className="header">
+          <PlayerCard player={player} showAmount={false} />
+          <Button bg="indianred" onClick={logOut}>Log Out</Button>
+        </div>
+
+        <div className="buttons">
+          <Link to="/join-game"><HomeButton bg="green">Join Game</HomeButton></Link>
+          <Link to="/create-game"><HomeButton>Create Game</HomeButton></Link>
+        </div>
       </div>
     </div>
   )
